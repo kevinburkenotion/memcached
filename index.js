3043d35a07eab345bc0499a49ccf6ac468064169
Kevin Burke <kevin@makenotion.com>
2019-03-26 16:58:41 -0700

    add get and set and del

--- index.js
+++ index.js
@@ -3,33 +3,19 @@ const events = require('events')
 
 const Pool = require('pg-pool')
 
-const EOFMessageList = [
-    'END',
-    'STORED',
-    'NOT_STORED',
-    'NOT_FOUND',
-    'ERROR',
-    'EXISTS',
-    'TOUCHED',
-    'DELETED',
-];
+const EOFMessageList = {
+    'END': true,
+    'STORED': true,
+    'NOT_STORED': true,
+    'NOT_FOUND': true,
+    'ERROR': true,
+    'EXISTS': true,
+    'TOUCHED': true,
+    'DELETED': true,
+};
 
 const CRLF = '\r\n';
 const CRLF_LENGTH = CRLF.length;
-
-const isEOF = function(buffer) {
-    for (var i = 0; i < EOFMessageList.length; i++) {
-        const msg = EOFMessageList[i]
-        const index = buffer.indexOf(msg);
-        if (index === -1) {
-            continue
-        }
-        if (index + msg.length + CRLF_LENGTH === buffer.length) {
-            return true
-        }
-    }
-    return false
-}
 
 class Client extends events.EventEmitter {
     constructor(config) {
@@ -46,27 +32,26 @@ class Client extends events.EventEmitter {
             port: this.port,
         })
         this.socket.setNoDelay(true)
-        this.socket.on('error', (err) => {
+        const onError = (err) => {
             if (!cbCalled) {
                 cbCalled = true
             }
             cb(err)
-        })
-        this.socket.on('timeout', (err) => {
+        }
+        this.socket.on('error', onError)
+        const onTimeout = (err) => {
             if (!cbCalled) {
                 cbCalled = true
             }
             cb(err)
-        })
+        }
+        this.socket.on('timeout', onTimeout)
         this.socket.on('ready', () => {
+            this.socket.removeListener('error', onError)
+            this.socket.removeListener('timeout', onError)
             cbCalled = true
             cb(null, this)
         })
-    }
-
-    release(done) {
-        console.log('releasing')
-        this.socket.destroy(done)
     }
 
     end() {
@@ -75,17 +60,25 @@ class Client extends events.EventEmitter {
 
     command(s) {
         return new Promise((resolve, reject) => {
+            const onError = (err) => {
+                this.socket.removeListener('error', onError)
+                this.socket.removeListener('data', parseResponse)
+                this.socket.destroy(err)
+                reject(err)
+            }
             const bufs = []
             let size = 0
             const parseResponse = (chunk) => {
                 bufs.push(chunk)
                 size += chunk.length
                 if (isEOF(chunk)) {
+                    this.socket.removeListener('error', onError)
                     this.socket.removeListener('data', parseResponse)
                     resolve(Buffer.concat(bufs, size))
                 }
             }
             this.socket.on('data', parseResponse)
+            this.socket.on('error', onError)
             let buf = Buffer.concat([Buffer.from(s, 'utf8'), Buffer.from(CRLF)])
             this.socket.write(buf)
         })
@@ -104,6 +97,27 @@ const poolFactory = (Client) => {
   return BoundPool
 }
 
+const spaceChar = ' '.charCodeAt(0)
+const returnChar = '\r'.charCodeAt(0)
+
+const getCode = function(buf) {
+    let idx = 0;
+    while (idx < buf.length && buf[idx] !== spaceChar && buf[idx] !== returnChar) {
+        idx++
+    }
+    return buf.slice(0, idx).toString()
+}
+
+const isEOF = function(buffer) {
+    // might not be at the beginning... if result is GET the end will be near
+    // the end.
+    const code = getCode(buffer)
+    if (code === 'VALUE') {
+        return buffer.indexOf('\r\nEND\r\n') !== -1
+    }
+    return EOFMessageList[code] === true
+}
+
 module.exports = class MemcachedPool {
     constructor(host, port) {
         var config = Object.assign({Client: Client}, {host: host, port: port})
@@ -114,6 +128,62 @@ module.exports = class MemcachedPool {
         const client = await this.pool.connect()
         const result = await client.command('get ' + key)
         await client.release()
+        const code = getCode(result)
+        switch (code) {
+            case 'END':
+                return null
+            case 'ERROR':
+                throw new Error(`get ${key}: Received ERROR response from Memcached server`)
+            case 'CLIENT_ERROR':
+                throw new Error(`get ${key}: got client error, check input: ${result.toString()}`)
+            case 'VALUE':
+                let start = result.indexOf(CRLF);
+                const meta = result.slice(0, start).toString('utf8').split(' ');
+                start += CRLF_LENGTH;
+                const value = result.slice(start, start + parseInt(meta[3], 10));
+                return value
+            default:
+                throw new Error(`get ${key}: unknown response ${code}`)
+        }
+        return result
+    }
+
+    async set(key, val, lifetimeSeconds) {
+        const client = await this.pool.connect()
+        const data = `set ${key} 0 ${lifetimeSeconds.toString()} ${val.length}\r\n${val}`
+        const result = await client.command(data)
+        await client.release()
+        const code = getCode(result)
+        switch (code) {
+            case 'STORED':
+                return 'STORED'
+            // item you are trying to store with a 'cas' command has been modified
+            case 'EXISTS':
+                return 'EXISTS'
+            case 'NOT_STORED':
+                throw new Error(`Data was not stored, but should have been (did not use add/replace to set it)`)
+            case 'CLIENT_ERROR':
+                throw new Error(`set ${key}: got client error, check input: ${result.toString()}`)
+            default:
+                throw new Error(`set ${key}: unknown response ${code}`)
+        }
+        return result
+    }
+
+    async delete(key) {
+        const client = await this.pool.connect()
+        const data = `delete ${key}`
+        const result = await client.command(data)
+        await client.release()
+        const code = getCode(result)
+        switch (code) {
+            case 'DELETED':
+                return 'DELETED'
+            case 'NOT_FOUND':
+                return 'EXISTS'
+            default:
+                throw new Error(`delete ${key}: unknown response ${code}`)
+        }
         return result
     }
 
--- test/test.js
+++ test/test.js
@@ -2,13 +2,51 @@ const expect = require('expect')
 
 const MemcachedPool = require('..')
 
+const timeoutPromise = function(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms)
+    })
+}
+
 describe('memcached', () => {
     describe('get', () => {
-        it('makes get requests', async () => {
+        it('returns null if a key is not present', async () => {
             const pool = new MemcachedPool('localhost', 11211)
             const result = await pool.get('ping')
-            expect(result.toString()).toBe('END\r\n')
+            expect(result).toBe(null)
             await pool.end()
         })
+
+        it('does not get expired items', async () => {
+            const pool = new MemcachedPool('localhost', 11211)
+            const randKey = "test-"+Math.floor(Math.random()*1000000).toString()
+            await pool.set(randKey, 'foo', 1)
+            await timeoutPromise(1001)
+            const val = await pool.get(randKey)
+            expect(val).toBe(null)
+            await pool.end()
+        })
+    })
+
+    describe('set', () => {
+        it('sets data', async () => {
+            const pool = new MemcachedPool('localhost', 11211)
+            const randKey = "test-"+Math.floor(Math.random()*1000000).toString()
+            const result = await pool.set(randKey, 'foo', 5000)
+            expect(result.toString()).toBe('STORED')
+            await pool.end()
+        })
+
+        it('can retrieve set data', async () => {
+            const pool = new MemcachedPool('localhost', 11211)
+            const randKey = "test-"+Math.floor(Math.random()*1000000).toString()
+            await pool.set(randKey, 'foo', 5000)
+            const val = await pool.get(randKey)
+            expect(val.toString()).toBe('foo')
+            await pool.end()
+        })
+    })
+
+    describe('delete', () => {
     })
 })
